524429c0093815c1b32f5be5679551d156ffb34a
pdmashkov <pdmashkov@rolftech.ru>
2024-11-24 15:00:04 +0300

    add Epic

---
+++ src/main/java/ru/netology/homeworkInherit/Epic.java
@@ -0,0 +1,20 @@
+package ru.netology.homeworkInherit;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (int i = 0; i < subtasks.length; i++) {
+            if (subtasks[i].contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
