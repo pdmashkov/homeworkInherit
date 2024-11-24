ef02e5c24ecbc4b0601f3cc0d6c8a077ea3e132f
pdmashkov <pdmashkov@rolftech.ru>
2024-11-24 15:00:14 +0300

    add SimpleTask

---
+++ src/main/java/ru/netology/homeworkInherit/SimpleTask.java
@@ -0,0 +1,19 @@
+package ru.netology.homeworkInherit;
+
+public class SimpleTask extends Task {
+    protected String title;
+
+    public SimpleTask(int id, String title) {
+        super(id);
+        this.title = title;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        if (title.contains(query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
